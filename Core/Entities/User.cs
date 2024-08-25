cafb3eb4e6ea32d4150cd6c8f9d90b822684b981
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-25 16:24:29 +0700

    add core/entities

---
+++ Core/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using CleanBase.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+	public class User : EntityNameAuditActive
+	{
+		public int Age { get; set; }
+	}
+}
