ccbaa0743ac59da732f8e765549752b9103bb282
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-25 16:25:01 +0700

    add core/services

---
+++ Core/Services/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using CleanBase.Core.Services.Core.Generic;
+using Core.Entities;
+using Core.ViewModels.Requests.User;
+using Core.ViewModels.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+	public interface 
+		IUserService : IServiceBase<User, UserRequest, UserResponse, UserGetAllRequest>
+	{
+		public Task<UserResponse> GetUserRandomMessage(Guid id);
+
+		public Task<bool> IsValidHuman(UserRequest userRequest);
+		Task TriggerUpdateUserAge();
+	}
+}
