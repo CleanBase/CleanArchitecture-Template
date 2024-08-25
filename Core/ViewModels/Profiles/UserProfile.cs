c0837782a07128974ea7981c27709b7f7be234e8
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-25 16:25:39 +0700

    add core/viewmodels

---
+++ Core/ViewModels/Profiles/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using CleanBase.Core.ViewModels.Profiles;
+using Core.Entities;
+using Core.ViewModels.Requests.User;
+using Core.ViewModels.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels.Profiles
+{
+	public partial class UserProfile : ProfileBase
+	{
+		protected override void DefaultMapping()
+		{
+			CreateMap<UserRequest, User>();
+			CreateMap<User, UserResponse>();
+			CreateMap<UserRequest, UserResponse>();	
+		}
+	}
+}
---
+++ Core/ViewModels/Responses/GetUserResponse.cs
@@ -0,0 +1,15 @@
+﻿using CleanBase.Core.ViewModels.Response.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels.Responses
+{
+	public class GetUserResponse : EntityResponseBase
+	{
+		public int Age { get; set; }
+		public string Message { get; set; }
+	}
+}
